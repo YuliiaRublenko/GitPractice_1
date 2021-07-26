c4eb1aa8f17e3e3eb5f798f780f75597bfb19508
YuliiaRublenko <87611408+YuliiaRublenko@users.noreply.github.com>
2021-07-26 11:30:29 -0400

    work 3 is done

--- src/day1/Second.java
+++ src/day1/Second.java
@@ -2,8 +2,8 @@ package day1;
 
 public class Second {
     public static void main(String[] args) {
-
+        System.out.println("work 2");
         //work 1 is done here
-
+//work 3 is done here
     }
 }
