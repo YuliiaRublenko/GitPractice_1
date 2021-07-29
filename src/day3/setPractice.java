40d9769d0141ae78febb0003502bbee8fe0b65ee
YuliiaRublenko <87611408+YuliiaRublenko@users.noreply.github.com>
2021-07-29 14:47:09 -0400

    third set commit

--- src/day3/setPractice.java
+++ src/day3/setPractice.java
@@ -3,4 +3,5 @@ package day3;
 public class setPractice {
 // set is a collection type that only store item
     //if you add duplicate - you can remove it easily
+    //it's a collection of unique elements
 }
