50d7857eef65e666cd5590214216f2400a59bfda
YuliiaRublenko <87611408+YuliiaRublenko@users.noreply.github.com>
2021-07-29 14:32:36 -0400

    forthChange

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -2,7 +2,8 @@ package day3;
 
 public class ListPractice {
     public static void main(String[] args) {
-//doing some great work here
+        //doing some great work here
         //some more changes are done
+        //and more...
     }
 }
